59d1747dd34860946d5e218cd1b929893f70f0a0
KossmoHard <zvladsz@gmail.com>
2019-05-23 12:22:14 +0300

    Commit2

--- selenium_test.py
+++ selenium_test.py
@@ -9,11 +9,12 @@ driver.get("https://www.google.com/")
 search = driver.find_element_by_name("q")
 search.clear()
 search.send_keys("news", Keys.ENTER)
-driver.implicitly_wait(10)
+#driver.implicitly_wait(10)
 
+desc = driver.find_elements_by_css_selector("html body div.srg div.g .rc > .s > div > span.st")
+link_title = driver.find_elements_by_css_selector("html body div.srg div.g .rc > .r h3")
+url = driver.find_elements_by_css_selector("html body div.srg div.g .rc > .r > a:not(.fl)")
 
-link_text = driver.find_elements_by_css_selector("html body div.srg div.g .rc > .r h3")
-for i in link_text:
-    print(i.text)
+for i in range(len(link_title)):
+    print("Title: {} \nDescription: {} \nLink: {}".format(link_title[i].text, desc[i].text, url[i].get_attribute("href")))
     print('-------------------------')
-
---
+++ test.py
@@ -0,0 +1,43 @@
+class Person:
+    name = "Vlad"
+    age = 20
+    gender = "M"
+
+    def __init__(self, name):
+        self.name = name
+
+    def set(self, name):
+        self.name = name
+
+    def __str__(self):
+        return "Name: {}".format(self.name)
+
+
+class Student(Person):
+    course = 1
+
+    # Переопределение метода set
+    def set(self, name, course):
+        self.name = name
+        self.course = course
+
+    # Переопределение магического метода __str__
+    def __str__(self):
+        return "Name: {} \nCourse: {}".format(self.name, self.course)
+
+
+person1 = Person("Andrey")
+print(person1)
+print("------------")
+
+person1.set("Alex")
+print(person1)
+print("------------")
+
+person2 = Student("Andrey")
+print(person2)
+print("------------")
+
+person2.set("Artur", 3)
+print(person2)
+print("------------")
