0c0c5e9ce173049a61224ec7ea283450de99af5d
KossmoHard <zvladsz@gmail.com>
2019-05-24 14:23:37 +0300

    Chromedriver patch

--- selenium_test.py
+++ selenium_test.py
@@ -1,15 +1,13 @@
-import os
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome(os.environ['PYTHONPATH'] + '/venv/bin/chromedriver')
+driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
 driver.get("https://www.google.com/")
 
 search = driver.find_element_by_name("q")
 search.clear()
 search.send_keys("news", Keys.ENTER)
-#driver.implicitly_wait(10)
+
 
 desc = driver.find_elements_by_css_selector("html body div.srg div.g .rc > .s > div > span.st")
 link_title = driver.find_elements_by_css_selector("html body div.srg div.g .rc > .r h3")
